8dbb938ff4f483e0bd6d00d9ab8a5c177626ddc2
Nishchal Kafle <Nishchal Kafle>
2016-05-24 17:59:03 +0545

    First commit

---
+++ Sum/src/com/nishchal/Java/Sum.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nishchal.Java;
+
+/**
+ *
+ * @author Nishchal
+ */
+public class Sum {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    int sum=0;
+      for (int i = 2;i<= 2;i++) {
+          for (int j = 1; j<=10; j++){
+              System.out.println(i*j);
+             sum += (j*i);
+          }}
+          System.out.println("Result=" + sum);
+          System.out.println("=============");
+          // TODO code application logic here
+    
+}
